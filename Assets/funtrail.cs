558e1864a0f4e63d315222f78a2f23a6bcc76d37
Wang Shuhan <wshuh126@gmail.com>
2018-06-02 15:09:46 +0800

    script added

---
+++ Assets/funtrail.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Finch;
+using UnityEngine;
+public class FinchFunTrail : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	public TrailRenderer kk;
+	public FinchChirality Chirality;
+
+	void Update () {
+		FinchController controller = FinchVR.GetFinchController (Chirality);
+
+		if (Input.GetKeyDown (KeyCode.A) || controller.GetPressDown (FinchControllerElement.ButtonThumb)) {
+			kk.enabled = false;
+		}
+	}
+
+}
